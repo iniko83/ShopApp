3740de34323a6357ab17bf3f1919648a2d07c8ac
Igor Nikolaev <oesabygames@gmail.com>
2025-08-01 12:24:49 +0400

    [LocalPackages] add NetworkClient.

---
+++ LocalPackages/NetworkClient/Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "NetworkClient",
+  platforms: [.iOS(.v17)],
+  products: [
+    .library(
+      name: "NetworkClient",
+      targets: ["NetworkClient"]
+    ),
+  ],
+  dependencies: [
+    .package(path: "../RestClient"),
+  ],
+  targets: [
+    .target(
+      name: "NetworkClient",
+      dependencies: [
+        .product(name: "RestClient", package: "RestClient"),
+      ]
+    ),
+  ]
+)
