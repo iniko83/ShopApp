44b9a8838efd787766a564b43f6b2cd11c64ed2d
Igor Nikolaev <oesabygames@gmail.com>
2025-08-01 10:03:37 +0400

    add empty project.

---
+++ Sources/Features/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  ShopApp
+//
+//  Created by Igor Nikolaev on 01.08.2025.
+//
+
+import SwiftUI
+
+struct RootView: View {
+  var body: some View {
+    Text("Root")
+  }
+}
+
+#Preview {
+  RootView()
+}
---
+++ Sources/StartUp/Application.swift
@@ -0,0 +1,17 @@
+//
+//  Application.swift
+//  ShopApp
+//
+//  Created by Igor Nikolaev on 01.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct Application: App {
+  var body: some Scene {
+    WindowGroup {
+      RootView()
+    }
+  }
+}
